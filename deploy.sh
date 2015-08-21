772886cf7edb6e12ce9bad3a2dc5b9f44953b2c6
heyu <hey@vmware.com>
2015-08-21 01:25:51 -0700

    add deploy.sh to setup vim/git/shell configuration in newly installed OS.

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#########################################################################
+# File Name:deploy.sh
+# Author:hey
+#mail: hey@vmware..com
+# Created Time:Fri 21 Aug 2015 01:20:27 AM PDT
+#########################################################################
+#!/bin/bash
+cur_dir=`pwd`
+ln -s $cur_dir/vim/.vimrc ~/.vimrc
+ln -s $cur_dir/vim/.vim ~/.vim
+ln -s $cur_dir/git/.gitconfig ~/.gitconfig
+ln -s $cur_dir/shell/.bash_profile ~/.bash_profile
+
