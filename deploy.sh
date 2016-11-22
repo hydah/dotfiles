f7a51ba62560ce4b8aa6452ecf51288ff4fdd35f
heyu <hey@vmware.com>
2016-11-22 11:57:47 +0800

    add consolas font

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,14 @@ ln -s $cur_dir/git/.gitconfig ~/.gitconfig
 ln -s $cur_dir/shell/.bash_profile ~/.bash_profile
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
+sudo mkdir /usr/share/fonts/consolas
+sudo cp $cur_dir/fonts/* /usr/share/fonts/consolas
+cd /usr/share/fonts/consolas
+sudo mkfontscale
+sudo mkfontdir
+sudo fc-cache -fv
+
+cd $cur_dir
+echo "source .bash_profile" >> ~/.bashrc
+source ~/.bashrc
+
