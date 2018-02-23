7ed14b4d5ad13e895856ab13937dcdc77834cbf6
heyu <hey@vmware.com>
2018-02-23 15:07:44 +0800

    update config for neovim

--- init-dev-env-termux.sh
+++ init-dev-env-termux.sh
@@ -11,6 +11,10 @@ ln -s $cur_dir/git/.gitconfig ~/.gitconfig
 ln -s $cur_dir/shell/.bash_profile ~/.bash_profile
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
+#for neovim
+ln -s $cur_dir/neovim/.config ~/.config
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 cd ~
 echo "source .bash_profile" >> ~/.bashrc
 source .bashrc
