e04943544302b7e563b652601e19d02914c513dd
heyu <hey@vmware.com>
2018-02-24 23:16:41 +0800

    update init-termux.sh

--- init-termux.sh
+++ init-termux.sh
@@ -5,6 +5,7 @@ pkg install vim-python
 pkg intall lua liblua-dev
 pkg install neovim
 pkg install clang libclang libllvm-dev lld llvm libllvm
+pip3 install neovim
 
 cur_dir=`pwd`
 ln -s $cur_dir/vim/.vimrc ~/.vimrc
@@ -13,8 +14,8 @@ ln -s $cur_dir/git/.gitconfig ~/.gitconfig
 ln -s $cur_dir/shell/.bash_profile ~/.bash_profile
 
 #for neovim
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s $cur_dir/neovim/.config ~/.config
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s $cur_dir/vim/.vimrc ~/.config/nvim/init.vim
 
 cd ~
