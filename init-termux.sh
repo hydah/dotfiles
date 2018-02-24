82ecc1a58596d34479778fea9e71c0e9431ad199
heyu <hey@vmware.com>
2018-02-24 22:41:25 +0800

    config for termux

--- init-termux.sh
+++ init-termux.sh
@@ -3,17 +3,19 @@
 pkg install python python2
 pkg install vim-python
 pkg intall lua liblua-dev
+pkg install neovim
+pkg install clang libclang libllvm-dev lld llvm libllvm
 
 cur_dir=`pwd`
 ln -s $cur_dir/vim/.vimrc ~/.vimrc
 ln -s $cur_dir/vim/.vim ~/.vim
 ln -s $cur_dir/git/.gitconfig ~/.gitconfig
 ln -s $cur_dir/shell/.bash_profile ~/.bash_profile
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 #for neovim
-ln -s $cur_dir/neovim/.config ~/.config
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s $cur_dir/neovim/.config ~/.config
+ln -s $cur_dir/vim/.vimrc ~/.config/nvim/init.vim
 
 cd ~
 echo "source .bash_profile" >> ~/.bashrc
